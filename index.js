bc67e1632332fa568092d4a0739770542cce8a31
Ignacio Paz <ignaciopaz72@gmail.com>
2021-02-21 17:20:59 -0400

    Generacion de enemigos y proyectiles

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const projectileVelocity = 5;
+
 class Player {
   constructor(x, y, radius, color) {
     this.x = x;
@@ -13,15 +15,110 @@ class Player {
     ctx.fill();
   }
 }
+class Projectile {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
+class Enemy {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+
+  update() {
+    this.draw();
+    this.x = this.x + this.velocity.x;
+    this.y = this.y + this.velocity.y;
+  }
+}
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.height = innerHeight;
 canvas.width = innerWidth;
 
-
 const x = canvas.width / 2;
 const y = canvas.height / 2;
 
 const player = new Player(x, y, 30, "#7489D9");
-player.draw();
+
+const projectiles = [];
+const enemies = [];
+//Evento cuando doy clik
+addEventListener("click", (event) => {
+  const angle = Math.atan2(
+    event.clientY - canvas.height / 2,
+    event.clientX - canvas.width / 2
+  );
+
+  const velocity = {
+    x: Math.cos(angle) * projectileVelocity,
+    y: Math.sin(angle) * projectileVelocity,
+  };
+
+  projectiles.push(
+    new Projectile(canvas.width / 2, canvas.height / 2, 10, "white", velocity)
+  );
+});
+
+const spawnEnemies = () => {
+  setInterval(() => {
+    const radius = (Math.random() * 15) + 15;
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const color = "green";
+
+    const angle = Math.atan2(canvas.height / 2 - y, canvas.width / 2 - x);
+
+    const velocity = {
+      x: Math.cos(angle) * 2,
+      y: Math.sin(angle) * 2,
+    };
+    enemies.push(new Enemy(x, y, radius, color, velocity));
+    console.log(enemies);
+  }, 1000);
+};
+//Reloj principal
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  projectiles.forEach((projectile) => {
+    projectile.update();
+  });
+
+  enemies.forEach((enemy) => {
+    enemy.update();
+  });
+
+  player.draw();
+};
+animate();
+spawnEnemies();
