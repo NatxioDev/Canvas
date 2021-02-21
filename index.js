704e6f71f055fd9f0b6bc0a6b749e5ac9b3c2add
Ignacio Paz <ignaciopaz72@gmail.com>
2021-02-21 13:30:01 -0400

    Arreglo de bugs y creacion de Jugador

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const draw = () => {
-  const canvas = document.querySelector("canvas");
-  const ctx = canvas.getContext("2d");
-  console.log("object");
-};
+class Player {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
 
-draw();
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+}
+
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.height = innerHeight;
+canvas.width = innerWidth;
+
+
+const x = canvas.width / 2;
+const y = canvas.height / 2;
+
+const player = new Player(x, y, 30, "#7489D9");
+player.draw();
